e10327deaabeb5a6866423018e511b526744a478
git.name <ghudeihed@newrelic.com>
2024-05-26 11:44:49 -0700

    adding three slave agents

--- init.groovy.d/basic-setup.groovy
+++ init.groovy.d/basic-setup.groovy
@@ -1,7 +1,9 @@
 import jenkins.model.*
 import hudson.model.*
+import hudson.slaves.*
+import jenkins.slaves.*
 
-println "Initial Jenkins Setup"
+println "--> Configuring Jenkins with custom setup"
 
 def instance = Jenkins.getInstance()
 
@@ -9,7 +11,36 @@ def instance = Jenkins.getInstance()
 instance.setNumExecutors(2)
 
 // Configure the system message
-instance.setSystemMessage("Welcome Jenkins Instance!")
+instance.setSystemMessage("Welcome to your custom Jenkins instance!")
 
 // Save the state
 instance.save()
+
+// Add slave nodes
+def nodes = [
+    [name: 'slave1', description: 'Slave Node 1', remoteFS: '/home/jenkins', numExecutors: 1],
+    [name: 'slave2', description: 'Slave Node 2', remoteFS: '/home/jenkins', numExecutors: 1],
+    [name: 'slave3', description: 'Slave Node 3', remoteFS: '/home/jenkins', numExecutors: 1]
+]
+
+nodes.each { node ->
+    try {
+        def launcher = new JNLPLauncher()
+        def slave = new DumbSlave(
+            node.name,
+            node.description,
+            node.remoteFS,
+            node.numExecutors.toString(),
+            Node.Mode.NORMAL,
+            '',
+            launcher,
+            new RetentionStrategy.Always(),
+            []
+        )
+        instance.addNode(slave)
+        println "Successfully added node: ${node.name}"
+    } catch (Exception e) {
+        println "Error adding node: ${node.name}"
+        e.printStackTrace()
+    }
+}
